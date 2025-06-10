db2e5df969fa8b11b5080308b778c7d148a9f859
Pavel <pavel@teroschin.com>
2025-06-10 23:44:52 +0200

    revert

--- producer-service/src/main/java/md/teroschin/kafkaproducer/controller/RecommendationController.java
+++ producer-service/src/main/java/md/teroschin/kafkaproducer/controller/RecommendationController.java
@@ -1,7 +1,6 @@
 package md.teroschin.kafkaproducer.controller;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import md.teroschin.kafkaproducer.dto.RecommendResponseDto;
 import md.teroschin.kafkaproducer.service.RecommendationService;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,9 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
-@Slf4j
 @RestController
 @RequestMapping("/recommendation")
 @RequiredArgsConstructor
@@ -22,9 +18,6 @@ public class RecommendationController {
     @GetMapping
     public RecommendResponseDto recommend(@RequestParam("userId") String userId,
                                           @RequestParam("productId") String productId) {
-        System.out.println("Received userId=" + userId + ", productId=" + productId);
-//        RecommendResponseDto recommendations = recommendationService.getRecommendations(userId, productId);
-//        System.out.println("Returned: " + recommendations);
-        return new RecommendResponseDto(List.of("user1", "user2", "user3"));
+        return recommendationService.getRecommendations(userId, productId);
     }
 }
