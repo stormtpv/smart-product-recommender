e2cff037ed88c93ac600471255da8f1aa54602b5
Pavel <pavel@teroschin.com>
2025-06-10 23:33:51 +0200

    add debug logs for producer

--- producer-service/src/main/java/md/teroschin/kafkaproducer/controller/RecommendationController.java
+++ producer-service/src/main/java/md/teroschin/kafkaproducer/controller/RecommendationController.java
@@ -1,6 +1,7 @@
 package md.teroschin.kafkaproducer.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import md.teroschin.kafkaproducer.dto.RecommendResponseDto;
 import md.teroschin.kafkaproducer.service.RecommendationService;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/recommendation")
 @RequiredArgsConstructor
@@ -18,6 +20,9 @@ public class RecommendationController {
     @GetMapping
     public RecommendResponseDto recommend(@RequestParam("userId") String userId,
                                           @RequestParam("productId") String productId) {
-        return recommendationService.getRecommendations(userId, productId);
+        log.info("Received userId=" + userId + ", productId=" + productId);
+        RecommendResponseDto recommendations = recommendationService.getRecommendations(userId, productId);
+        log.info("Returned: " + recommendations);
+        return recommendations;
     }
 }
