be64a943947f65f39dc41c9ff1263c4e291411f6
Pavel <pavel@teroschin.com>
2025-06-13 23:02:14 +0200

    add observability

--- ml-service/app.py
+++ ml-service/app.py
@@ -1,12 +1,25 @@
 from fastapi import FastAPI, Query
 from typing import List
 from pydantic import BaseModel
+from prometheus_client import Counter, generate_latest
+from fastapi.responses import Response
 import random
 
 app = FastAPI()
 
+REQUEST_COUNT = Counter('request_count', 'Total request count')
+
 class RecommendResponse(BaseModel):
     recommendations: List[str]
+
+@app.get("/")
+async def root():
+    REQUEST_COUNT.inc()
+    return {"Hello": "World"}
+
+@app.get("/metrics")
+async def metrics():
+    return Response(generate_latest(), media_type="text/plain")
 
 @app.get("/recommend", response_model=RecommendResponse)
 def recommend(user_id: str = Query(...), product_id: str = Query(...)):
